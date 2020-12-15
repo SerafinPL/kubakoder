8cfabf1f22a15e43cd903162f70174b463ed72a1
Serafinpl <ser.burbrbar@gmail.com>
2020-12-15 17:14:49 +0100

    czasy skonczone

--- js/czas.js
+++ js/czas.js
@@ -1,9 +1,35 @@
 
 
-var dd = new Date();
-var e = dd.getFullYear() + '/';
-var month = dd.getMonth() + 1;
-e += month + '/'
+setInterval(czasownik, 1000);
+
+function czasownik(){
+
+  var dd = new Date();
+
+  var e = dd.getHours() + ':';
+  var minut = dd.getMinutes();
+  if (minut < 10) {
+    e += '0';
+  }
+  e += minut + ':';
+  var secondo = dd.getSeconds();
+  if (secondo < 10) {
+    e += '0';
+  }
+  e += secondo + '     ';
+
+  e += dd.getDate() + '/';
+  var miesiac = dd.getMonth() + 1;
+  if (miesiac < 10) {
+    e += '0';
+  }
+  e += miesiac + '/';
+  e += dd.getFullYear() + 'r';
 
 
-document.getElementById('czas').innerHTML = e ;
+
+  document.getElementById('czas').innerHTML = e ;
+}
+
+
+czasownik();
